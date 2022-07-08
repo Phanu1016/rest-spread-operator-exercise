77a14271988b2b336397ebb81b76a62a7eb66a46
Phanu <admin@phanuphanu.com>
2022-07-08 11:37:06 -0700

    doubleAndReturnArgs()

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const filterOutOdds = (...nums) => nums.filter( (num) => num % 2 === 0)
 const findMin = (...nums) => nums.reduce( (lowest, current) => current > lowest ? lowest : current)
 
 const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2}) 
+
+const doubleAndReturnArgs = (arr, ...nums) => [...arr, ...nums.map((value) => value * 2)]
