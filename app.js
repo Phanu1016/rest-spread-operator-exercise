7e8a825bf30ebf794fe022f4187d1c7d59cf63a8
Phanu <admin@phanuphanu.com>
2022-07-08 11:32:06 -0700

    mergeObjects()

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const filterOutOdds = (...nums) => nums.filter( (num) => num % 2 === 0)
 
 const findMin = (...nums) => nums.reduce( (lowest, current) => current > lowest ? lowest : current)
+
+const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2}) 
